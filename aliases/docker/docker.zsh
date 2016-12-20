0d66712d0a57abe3c6c1d7be7e49b5420a1a7b24
Hưng Đinh Văn <dinhhung@it-s.vn>
2016-12-20 17:03:05 +0700

    update docker aliases

--- aliases/docker/docker.zsh
+++ aliases/docker/docker.zsh
@@ -26,21 +26,21 @@ alias dex="docker exec -i -t"
 alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
 
 # Stop all containers
-dstop() { docker stop $(docker ps -a -q); }
+alias dstop="docker stop $(docker ps -a -q)"
 
 # Remove all containers
-drm() { docker rm $(docker ps -a -q); }
+alias drm="docker rm $(docker ps -a -q)"
 
 # Remove all images
-dri() { docker rmi $(docker images -q); }
+alias dri="docker rmi $(docker images -q)"
 
 # Dockerfile build, e.g., $dbu tcnksm/test
-dbu() { docker build -t=$1 .; }
+alias dbu="docker build -t=$1 ."
 
 # Show all alias related docker
 dali() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
 
 # Bash into running container
-dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+alias dbash="docker exec -it $(docker ps -aqf "name=$1") bash"
 
 # Docker Compose
